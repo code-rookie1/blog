88cc9d8ef6411d3f1928128ce4ea52623b340623
code-rookie1 <wa1kman@163.com>
2021-01-20 21:53:57 +0800

    【修改】修复模型关系错误

--- backend/model/label.go
+++ backend/model/label.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Label struct {
 	gorm.Model
-	Name        string     `json:"name" gorm:"comment:标签名称"`
-	Alias       string     `json:"alias" gorm:"comment:标签别名"`
-	Description string     `json:"description" gorm:"type:text;comment:标签描述"`
-	Article     []*Article `json:"article" gorm:"many2many:article_label;comment:文章"`
+	Name        string    `json:"name" gorm:"comment:标签名称"`
+	Alias       string    `json:"alias" gorm:"comment:标签别名"`
+	Description string    `json:"description" gorm:"type:text;comment:标签描述"`
+	Article     []Article `json:"article" gorm:"comment:文章"` //一个标签有多篇文章
 }
